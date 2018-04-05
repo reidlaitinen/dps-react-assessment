ccf087f59416e07e67aaf58e404b18f2e590a0e9
Reid Laitinen <reidlaitinen@gmail.com>
2018-04-05 09:43:58 -0600

    working on infinite scroll

--- client/src/components/Beers.js
+++ client/src/components/Beers.js
@@ -17,38 +17,60 @@ class Beers extends React.Component {
   state = { visible: [], page: 1, totalPages: 0 }
 
   componentDidMount() {
-    axios.get(`/api/all_beers?per_page=${10}`).then((res) => {this.setState({ visible: res.data.entries }) })
+    axios.get(`/api/all_beers?per_page=${10}`).then((res) => {this.setState({ totalPages: 7295, visible: res.data.entries }) })
+  }
+
+  loadMore = () => {
+    const page = this.state.page + 1;
+    axios.get(`/api/all_beers?page=${page}&per_page=${10}`)
+    .then(res => { this.setState(
+        { visible: [...this.state.visible, ...res.data.entries],
+          page: this.state.page + 1,
+        }
+      );
+      }
+      );
   }
 
   render() {
-    const { visible } = this.state
+    const { visible, page, totalPages } = this.state
     return (
       <div>
         <Header style={styles.masthead}>All Beers</Header>
         <Divider hidden />
         <Grid>
           <Grid.Column>
-            <Table centered striped>
-              <Table.Header>
-                <Table.HeaderCell>Icon</Table.HeaderCell>
-                <Table.HeaderCell>Name</Table.HeaderCell>
-                <Table.HeaderCell>Style</Table.HeaderCell>
-                <Table.HeaderCell>ABV</Table.HeaderCell>
-              </Table.Header>
-              <Table.Body>
-                {visible.map( entry => (   
+            <InfiniteScroll
+                    pageStart={page}
+                    loadMore={this.loadMore}
+                    hasMore={page < totalPages}
+                    useWindow={false}
+            >
+              <Table centered striped>
+                <Table.Header>
+                  <Table.HeaderCell>Icon</Table.HeaderCell>
+                  <Table.HeaderCell>Name</Table.HeaderCell>
+                  <Table.HeaderCell>Style</Table.HeaderCell>
+                  <Table.HeaderCell>ABV</Table.HeaderCell>
+                </Table.Header>
+                <Table.Body>
+                  {visible.map( entry => (   
                   <Table.Row>
                     <Table.Cell>
                       {entry.hasOwnProperty('labels') && <Image src={entry.labels.icon} />}
                     </Table.Cell>
-                    <Table.Cell>{entry.name_display}</Table.Cell>
-                    <Table.Cell>{entry.style.name}</Table.Cell>
-                    <Table.Cell>{entry.abv} %</Table.Cell>
+                    <Table.Cell verticalAlign='middle'>{entry.name_display}</Table.Cell>
+                    {entry.hasOwnProperty('style') ? 
+                      <Table.Cell verticalAlign='middle'>{entry.style.name}</Table.Cell>
+                      :
+                      <Table.Cell disabled verticalAlign='middle'>No data</Table.Cell>
+                    }
+                    <Table.Cell verticalAlign='middle'>{entry.abv} %</Table.Cell>
                   </Table.Row>
-                  )
-                )}
-              </Table.Body>
-            </Table>
+                  ))}
+                </Table.Body>
+              </Table>
+            </InfiniteScroll>
           </Grid.Column>
         </Grid>
       </div>
