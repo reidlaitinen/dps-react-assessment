4c07f454ae6836ff1ca29e9415d44b229f39a872
Reid Laitinen <reidlaitinen@gmail.com>
2018-04-05 23:54:20 -0600

    beer info modal WIP

--- client/src/components/Beers.js
+++ client/src/components/Beers.js
@@ -8,13 +8,15 @@ import {
   Table,
   Divider,
   Image,
+  Modal,
+  Icon,
 } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import InfiniteScroll from 'react-infinite-scroller';
 
 class Beers extends React.Component {
 
-  state = { visible: [], page: 1, totalPages: 0 }
+  state = { visible: [], page: 1, totalPages: 0, modalOpen: false, modalBeer: {} }
 
   componentDidMount() {
     axios.get(`/api/all_beers?per_page=${10}`).then((res) => {this.setState({ totalPages: 7295, visible: res.data.entries }) })
@@ -28,59 +30,112 @@ class Beers extends React.Component {
           page: this.state.page + 1,
         }
       );
-      }
-      );
+    });
+  }
+
+  toggleModal = (beer = {}) => {
+    this.setState( state => {
+      return { modalOpen: !this.state.modalOpen, modalBeer: beer }
+    })
   }
 
   render() {
-    const { visible, page, totalPages } = this.state
+    let open = {}
+    if (this.state.modalOpen) 
+      open = { open: true }
+    
+    const { visible, page, totalPages, modalBeer } = this.state
+
     return (
       <div>
-        <Header style={styles.masthead}>All Beers</Header>
+        <Modal dimmer='blurring' style={styles.modal} {...open} onClose={() => this.toggleModal()}>
+          <Modal.Header>BEER INFO</Modal.Header>
+          <Modal.Content image scrolling>
+          {modalBeer.labels ? 
+            <Image
+              size='medium'
+              src={modalBeer.labels.medium}
+              wrapped
+            />
+          :
+            <Icon name='beer' size='massive'/>
+          }
+            <Modal.Description>
+              <Header>{modalBeer.name_display}</Header>
+              {modalBeer.hasOwnProperty('style')
+                ? <Header as='h4'>{modalBeer.style.name}</Header> 
+                : <Header as='h4'><i>no style indicated</i></Header>
+              }
+              {modalBeer.hasOwnProperty('style')
+                ? <p>{modalBeer.style.description}</p> 
+                : <p><i>no description</i></p>
+              }
+            </Modal.Description>
+          </Modal.Content>
+        </Modal>
+      <div>
+        <Header style={styles.mastHead}>All Beers</Header>
         <Divider hidden />
         <Grid>
           <Grid.Column>
-            <InfiniteScroll
-                    pageStart={page}
-                    loadMore={this.loadMore}
-                    hasMore={page < totalPages}
-                    useWindow={false}
-            >
-              <Table centered striped>
-                <Table.Header>
-                  <Table.HeaderCell>Icon</Table.HeaderCell>
-                  <Table.HeaderCell>Name</Table.HeaderCell>
-                  <Table.HeaderCell>Style</Table.HeaderCell>
-                  <Table.HeaderCell>ABV</Table.HeaderCell>
-                </Table.Header>
-                <Table.Body>
-                  {visible.map( entry => (   
-                  <Table.Row>
-                    <Table.Cell>
-                      {entry.hasOwnProperty('labels') && <Image src={entry.labels.icon} />}
-                    </Table.Cell>
-                    <Table.Cell verticalAlign='middle'>{entry.name_display}</Table.Cell>
-                    {entry.hasOwnProperty('style') ? 
-                      <Table.Cell verticalAlign='middle'>{entry.style.name}</Table.Cell>
-                      :
-                      <Table.Cell disabled verticalAlign='middle'>No data</Table.Cell>
-                    }
-                    <Table.Cell verticalAlign='middle'>{entry.abv} %</Table.Cell>
-                  </Table.Row>
-                  ))}
-                </Table.Body>
-              </Table>
-            </InfiniteScroll>
+            <Table>
+              <Table.Header>
+                <Table.HeaderCell>Icon</Table.HeaderCell>
+                <Table.HeaderCell>Name</Table.HeaderCell>
+                <Table.HeaderCell>Style</Table.HeaderCell>
+                <Table.HeaderCell>ABV</Table.HeaderCell>
+              </Table.Header>
+            </Table>
+              <div style={styles.scroller}>
+                <InfiniteScroll
+                  pageStart={page}
+                  loadMore={this.loadMore}
+                  hasMore={page < totalPages}
+                  useWindow={false}
+                >
+                <Table striped>
+                  <Table.Body>
+                    {visible.map( entry => (   
+                    <Table.Row key={entry.id} style={styles.pointer} onClick={ () => this.toggleModal(entry)}>
+                      <Table.Cell>
+                        {entry.hasOwnProperty('labels') ? <Image src={entry.labels.icon} /> : <Icon name='beer' size='huge'/>}
+                      </Table.Cell>
+                      <Table.Cell verticalAlign='middle'>{entry.name_display}</Table.Cell>
+                      {entry.hasOwnProperty('style') ? 
+                        <Table.Cell verticalAlign='middle'>{entry.style.name}</Table.Cell>
+                        :
+                        <Table.Cell disabled verticalAlign='middle'>No data</Table.Cell>
+                      }
+                      <Table.Cell verticalAlign='middle'>{entry.abv} %</Table.Cell>
+                    </Table.Row>
+                    ))}
+                  </Table.Body>
+                </Table>
+              </InfiniteScroll>
+            </div>
           </Grid.Column>
         </Grid>
       </div>
+    </div>
     )
   }
 }
 
 const styles = {
-  masthead: {
+  mastHead: {
     textAlign: 'center'
+  },
+  scroller: {
+    height: '70vh',
+    overflow: 'auto'
+  },
+  pointer: {
+    cursor: 'pointer'
+  },
+  modal : {
+    marginTop: '0px !important',
+    marginLeft: 'auto',
+    marginRight: 'auto'
   }
 }
 
--- notes.md
+++ notes.md
@@ -1,10 +1,24 @@
 Started: 2018-04-04 7:30 PM
 
-10:52 PM: Preparing Beers.js for infinite-scroll pagination.
+2018-04-04 10:52 PM: 
+  Preparing Beers.js for infinite-scroll pagination.
+
+2018-04-05 11:25 PM:
+  Beers component infinite scroll works
+  Show labels if they have them, icons if not
+  Click a row & get a modal that shows beer info
 
 
 
-TODOS:
-  Beer name search
-  Beer style filter
-  Beer minimum ABV filter
+TODOS (Little)
+  + Finish beer info modal info / styling
+  + Setup Breweries / info
+    - use cards instead of list?
+  + Fix beer list table header alignment
+  + Fix modal alignment
+  + ABV min/max avg if ABV not specified
+
+TODOS (Big)
+  + Beer name search
+  + Beer style filter
+  + Beer minimum ABV filter
